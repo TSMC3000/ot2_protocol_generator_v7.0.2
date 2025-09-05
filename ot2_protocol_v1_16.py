f053cbfd962f3dbcb4592f6f1af359eda0a671ff
TSMC3000 <102569674+TSMC3000@users.noreply.github.com>
2025-09-04 23:23:57 -0400

    Update ot2_protocol_v1_16.py
    change os path

--- ot2_protocol_v1_16.py
+++ ot2_protocol_v1_16.py
@@ -4,7 +4,8 @@ import sys
 import os
 
 # Add parent directory to path to import from the existing modules
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.abspath('.'))
 from matal.utils import get_sid
 
 # Page config
